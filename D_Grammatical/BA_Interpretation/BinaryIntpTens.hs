271e894218d3c246139d232380ea8fb9ca9d5f66
cherryfunk <d.p.schellhorn@gmail.com>
2026-03-21 15:44:21 -0600

    backup

--- D_Grammatical/BA_Interpretation/BinaryIntpTens.hs
+++ D_Grammatical/BA_Interpretation/BinaryIntpTens.hs
@@ -17,7 +17,7 @@ import C_Domain.C_TypeSystem.Tens (TENS (..))
 import C_Domain.B_Theory.BinaryTheory (BinaryFun (..), BinaryKlFun (..), BinarySorts (..))
 import C_Domain.BA_Interpretation.BinaryReal ()              -- BinaryFun/KlFun TENS
 import C_Domain.BA_Interpretation.BinaryRealMLP (ParamsMLP)
-import D_Grammatical.B_Theory.BinaryFormulas (binarySentence)
+import D_Grammatical.B_Theory.BinaryFormulas (binarySentenceTens)
 import qualified Torch
 import Torch.Typed.Tensor (Tensor (..), toDynamic)
 
@@ -25,4 +25,4 @@ import Torch.Typed.Tensor (Tensor (..), toDynamic)
 --     forall x. (label(x) -> pred(x)) /\ (not label(x) -> not pred(x))
 binaryAxiomTens :: Torch.Tensor -> Torch.Tensor -> ParamsMLP -> Omega TENS
 binaryAxiomTens betaT dataTensor paramMLP =
-  binarySentence @TENS betaT (TensorBatch dataTensor) paramMLP
+  binarySentenceTens @TENS betaT (TensorBatch dataTensor) paramMLP
